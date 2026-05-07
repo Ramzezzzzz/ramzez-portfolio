fdc040be48f35b12d15b2173de0327dab3a1be1d
Ramzez <135848865+Ramzezzzzz@users.noreply.github.com>
2026-05-07 15:10:37 +0000

    Fix gradient, parallax, and add image compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7, interlaced: false },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 80, progressive: true },
+      pngquant: { quality: [0.7, 0.9], speed: 4 },
+      svgo: { plugins: [{ removeViewBox: false }] },
+      webp: { quality: 80 },
+    }),
+  ],
   base: "/portfolio/",
 });
